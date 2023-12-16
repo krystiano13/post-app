3f51a6731f463229d9c046ede7635388e0888f07
krystiano13 <crystianotv@gmail.com>
2023-12-16 15:24:29 +0100

    create register page

--- client/app/register/page.js
+++ client/app/register/page.js
@@ -1,13 +1,16 @@
 import {Button, Input, FormLabel, Card} from "@chakra-ui/react";
-
-export default function Login() {
+export default function Register() {
     return (
         <div className="w-[100vw] h-[100vh] flex items-center text-start justify-center text-amber-50">
             <Card size="lg" variant="outline" className="p-6 rounded-full">
                 <form className="flex flex-col justify-center items-center text-start max-w-[20rem] w-[20rem]">
                     <FormLabel className="w-[20rem] pl-1.5 mt-6">Username</FormLabel>
                     <Input required type="text" />
+                    <FormLabel className="w-[20rem] pl-1.5 mt-6">Email Address</FormLabel>
+                    <Input name="email" required type="email" />
                     <FormLabel className="w-[20rem] pl-1.5 mt-6">Password</FormLabel>
+                    <Input required type="password" />
+                    <FormLabel className="w-[20rem] pl-1.5 mt-6">Repeat Password</FormLabel>
                     <Input required type="password" />
                     <Button colorScheme={"blue"} className="w-full mt-6" type="submit">Test</Button>
                 </form>
