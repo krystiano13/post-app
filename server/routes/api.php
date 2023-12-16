310b791c153de42a2ecbe76c92d7abf5f4943126
krystiano13 <crystianotv@gmail.com>
2023-12-16 19:58:00 +0100

    create routing for getting posts

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/posts', [PostController::class, 'get']);
+Route::get('/posts/{id}', [PostController::class, 'getOne']);
