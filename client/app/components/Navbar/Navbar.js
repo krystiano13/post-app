cbe8eef54859455571892182b399b6083197593a
krystiano13 <crystianotv@gmail.com>
2023-12-18 11:14:01 +0100

    create styles for navbar wrapper

--- client/app/components/Navbar/Navbar.js
+++ client/app/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 export function Navbar() {
     return (
-        <nav className="w-full h-[4rem] border-b-2 border-b-solid border-b-blue-600">
+        <nav className="w-full h-[4rem] fixed border-b-2 border-b-solid border-b-blue-300 border-opacity-40 bg-blue-400 bg-opacity-10">
 
         </nav>
     )
--- client/app/login/page.js
+++ client/app/login/page.js
@@ -2,7 +2,7 @@ import {Button, Input, FormLabel, Card} from "@chakra-ui/react";
 
 export default function Login() {
     return (
-        <div className="w-[full] h-wout-nav flex items-center text-start justify-center text-amber-50">
+        <div className="w-[full] pt-[4rem] h-wout-nav flex items-center text-start justify-center text-amber-50">
             <Card size="lg" variant="outline" className="p-6 rounded-full">
                 <form className="flex flex-col justify-center items-center text-start max-w-[20rem] w-[20rem]">
                     <FormLabel className="w-[20rem] pl-1.5 mt-6">Username</FormLabel>
