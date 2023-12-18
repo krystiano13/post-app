8b1af918e9a8bbc632d2d42afc408b23b988f0c1
krystiano13 <crystianotv@gmail.com>
2023-12-18 11:43:23 +0100

    create styles for mobile version of navbar (for unlogged user)

--- client/app/components/Navbar/Navbar.js
+++ client/app/components/Navbar/Navbar.js
@@ -1,17 +1,30 @@
-import { Button, Input, Heading } from "@chakra-ui/react";
+import { Button, Input, Heading, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 
 export function Navbar() {
     return (
         <nav className="flex p-2 items-center justify-between w-full h-[4rem] fixed border-b-2 border-b-solid border-b-blue-300 border-opacity-40 bg-blue-400 bg-opacity-10">
-            <div className="w-1/3 flex justify-start" id="logo">
-                <Heading as="h1" size="lg">POST APP</Heading>
+            <div className="md:w-1/3 w-1/5 flex items-center justify-start" id="logo">
+                <Heading as="h1" size="sm">
+                    <span className="text-[0.85rem] md:text-[1.25rem]">POST APP</span>
+                </Heading>
             </div>
-            <div className="w-1/3 flex justify-center" id="searchBar">
+            <div className="md:w-1/3 w-1/2 flex items-center justify-center" id="searchBar">
                 <Input name="search" type="text" placeholder="Search ..." />
             </div>
-            <div id="buttons" className="h-full w-1/3 flex justify-end">
+            <div id="buttons" className="h-full w-1/3 justify-end items-center hidden md:flex">
                 <Button colorScheme="blue" className="ml-1 mr-1">Log In</Button>
                 <Button colorScheme="gray" className="ml-1 mr-1">Create Account</Button>
+            </div>
+            <div className="block md:hidden w-1/4 flex items-center justify-end">
+                <Menu>
+                    <MenuButton as={Button}>
+                       <span className="text-[0.8rem] text-[1rem]">Account</span>
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>Log In</MenuItem>
+                        <MenuItem>Create Account</MenuItem>
+                    </MenuList>
+                </Menu>
             </div>
         </nav>
     )
