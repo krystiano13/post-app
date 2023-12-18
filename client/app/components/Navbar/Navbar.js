7701f37169f023a52f2ab90ffe06cfed89f05cde
krystiano13 <crystianotv@gmail.com>
2023-12-18 11:25:08 +0100

    create desktop view of navbar for unlogged user

--- client/app/components/Navbar/Navbar.js
+++ client/app/components/Navbar/Navbar.js
@@ -1,7 +1,18 @@
+import { Button, Input, Heading } from "@chakra-ui/react";
+
 export function Navbar() {
     return (
-        <nav className="w-full h-[4rem] fixed border-b-2 border-b-solid border-b-blue-300 border-opacity-40 bg-blue-400 bg-opacity-10">
-
+        <nav className="flex p-2 items-center justify-between w-full h-[4rem] fixed border-b-2 border-b-solid border-b-blue-300 border-opacity-40 bg-blue-400 bg-opacity-10">
+            <div className="w-1/3 flex justify-start" id="logo">
+                <Heading as="h1" size="lg">POST APP</Heading>
+            </div>
+            <div className="w-1/3 flex justify-center" id="searchBar">
+                <Input name="search" type="text" placeholder="Search ..." />
+            </div>
+            <div id="buttons" className="h-full w-1/3 flex justify-end">
+                <Button colorScheme="blue" className="ml-1 mr-1">Log In</Button>
+                <Button colorScheme="gray" className="ml-1 mr-1">Create Account</Button>
+            </div>
         </nav>
     )
 }
