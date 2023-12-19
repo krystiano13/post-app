c22896198cdb7ff0a83ea2ada3250e7037c6909f
krystiano13 <crystianotv@gmail.com>
2023-12-19 21:06:25 +0100

    implement redirecting to specific post

--- client/app/components/PostPreview/PostLIst.js
+++ client/app/components/PostPreview/PostLIst.js
@@ -1,6 +1,7 @@
 import { PostPreview } from "@/app/components/PostPreview/PostPreview";
 import { Spinner } from "@chakra-ui/react";
-import {Suspense} from "react";
+import { Suspense } from "react";
+import Link from 'next/link';
 
 async function getPosts(page = 0) {
     const res = await fetch(`http://127.0.0.1:8000/api/posts/latest/${page}`);
@@ -10,19 +11,21 @@ async function getPosts(page = 0) {
 export async function PostList() {
     const posts = await getPosts(0);
     return (
-        <>
+        <div className="w-[100vw]">
            <Suspense fallback={<Spinner />}>
                {
                    posts.data.map(item => (
-                       <PostPreview
-                           title={item.title}
-                           content={item.content}
-                           username={item.username}
-                           createdAt={item.created_at}
-                       />
+                       <Link className="w-[100vw] flex justify-center" href={`/posts/${item.id}`}>
+                           <PostPreview
+                               title={item.title}
+                               content={item.content}
+                               username={item.username}
+                               createdAt={item.created_at}
+                           />
+                       </Link>
                    ))
                }
            </Suspense>
-        </>
+        </div>
     )
 }
