0c658089c86812ddf6389b7082cbb79d68360e8b
krystiano13 <crystianotv@gmail.com>
2023-12-19 13:26:33 +0100

    implement fetching data

---
+++ client/app/components/PostPreview/PostLIst.js
@@ -0,0 +1,55 @@
+'use client'
+
+import { PostPreview } from "@/app/components/PostPreview/PostPreview";
+import { useEffect, useState } from 'react';
+import { Spinner, Text } from "@chakra-ui/react";
+
+export function PostList() {
+    const [ posts,setPosts ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(false);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/posts/latest/0')
+            .then(res => res.json())
+            .then(result => {
+                setPosts(result.data);
+            })
+            .then(() => {
+                setLoading(false);
+            })
+            .catch(() => {
+                setError(true);
+            })
+    },[])
+
+    return (
+        <>
+            {
+                loading === true && (
+                    <div className="w-full flex justify-center items-center mt-[2rem]">
+                        <Spinner />
+                    </div>
+                )
+            }
+            {
+                loading === false && (
+                    <>
+                        {
+                            posts.map(item => (
+                                <PostPreview />
+                            ))
+                        }
+                    </>
+                )
+            }
+            {
+                error === true && (
+                    <div className="w-full flex justify-center items-center mt-[2rem]">
+                        <Text>Couldn't download data from server</Text>
+                    </div>
+                )
+            }
+        </>
+    )
+}
--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,11 @@
 import { Button } from '@chakra-ui/react';
-import { PostPreview } from "@/app/components/PostPreview/PostPreview";
+import { PostList } from "@/app/components/PostPreview/PostList";
 
 export default function Home() {
   return (
     <div className="w-100vw h-auto pt-[5rem] flex flex-col items-center">
       <Button className="lg:w-1/2 md:w-3/4 w-[80%]">Create Post +</Button>
-      <PostPreview />
+      <PostList />
     </div>
   )
 }
