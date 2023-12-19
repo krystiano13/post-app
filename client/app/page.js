eaa01b9dadd12d1a041b99a5f47f562e7d0d96cc
krystiano13 <crystianotv@gmail.com>
2023-12-19 12:28:41 +0100

    create gradient to hide text in preview

--- client/app/page.js
+++ client/app/page.js
@@ -4,7 +4,7 @@ import { PostPreview } from "@/app/components/PostPreview/PostPreview";
 export default function Home() {
   return (
     <div className="w-100vw h-auto pt-[5rem] flex flex-col items-center">
-      <Button className="md:w-1/2 w-[80%]">Create Post +</Button>
+      <Button className="lg:w-1/2 md:w-3/4 w-[80%]">Create Post +</Button>
       <PostPreview />
     </div>
   )
