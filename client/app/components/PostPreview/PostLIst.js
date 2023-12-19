03739936366e8cc8ac7861a256dc77f6fdbee20b
krystiano13 <crystianotv@gmail.com>
2023-12-19 20:56:54 +0100

    improve data fetching performance in PostsList component'

--- client/app/components/PostPreview/PostLIst.js
+++ client/app/components/PostPreview/PostLIst.js
@@ -1,60 +1,28 @@
-'use client'
-
 import { PostPreview } from "@/app/components/PostPreview/PostPreview";
-import { useEffect, useState } from 'react';
-import { Spinner, Text } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+import {Suspense} from "react";
 
-export function PostList() {
-    const [ posts,setPosts ] = useState([]);
-    const [ loading, setLoading ] = useState(true);
-    const [ error, setError ] = useState(false);
+async function getPosts(page = 0) {
+    const res = await fetch(`http://127.0.0.1:8000/api/posts/latest/${page}`);
+    return res.json();
+}
 
-    useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/posts/latest/0')
-            .then(res => res.json())
-            .then(result => {
-                setPosts(result.data);
-            })
-            .then(() => {
-                setLoading(false);
-            })
-            .catch(() => {
-                setError(true);
-            })
-    },[])
-
+export async function PostList() {
+    const posts = await getPosts(0);
     return (
         <>
-            {
-                loading === true && (
-                    <div className="w-full flex justify-center items-center mt-[2rem]">
-                        <Spinner />
-                    </div>
-                )
-            }
-            {
-                loading === false && (
-                    <>
-                        {
-                            posts.map(item => (
-                                <PostPreview
-                                    title={item.title}
-                                    content={item.content}
-                                    username={item.username}
-                                    createdAt={item.created_at}
-                                />
-                            ))
-                        }
-                    </>
-                )
-            }
-            {
-                error === true && (
-                    <div className="w-full flex justify-center items-center mt-[2rem]">
-                        <Text>Couldn't download data from server</Text>
-                    </div>
-                )
-            }
+           <Suspense fallback={<Spinner />}>
+               {
+                   posts.data.map(item => (
+                       <PostPreview
+                           title={item.title}
+                           content={item.content}
+                           username={item.username}
+                           createdAt={item.created_at}
+                       />
+                   ))
+               }
+           </Suspense>
         </>
     )
 }
