db0283c9c52207ffc5fcd03781a753af2e1ba32c
krystiano13 <crystianotv@gmail.com>
2023-12-19 12:04:21 +0100

    create button to create post

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { Button } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="w-100vw h-auto pt-[5rem] flex flex-col items-center">
+      <Button className="md:w-1/2 w-[80%]">Create Post +</Button>
     </div>
   )
 }
