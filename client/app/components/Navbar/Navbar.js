df84326f65c9f78055df85261876b4a63894c59f
krystiano13 <crystianotv@gmail.com>
2023-12-20 12:04:08 +0100

    implement redirecting to home page

--- client/app/components/Navbar/Navbar.js
+++ client/app/components/Navbar/Navbar.js
@@ -5,9 +5,11 @@ export function Navbar() {
     return (
         <nav className="flex p-2 items-center justify-between w-full h-[4rem] fixed border-b-2 border-b-solid border-b-blue-300 border-opacity-40 bg-bl">
             <div className="md:w-1/3 w-1/5 flex items-center justify-start" id="logo">
-                <Heading as="h1" size="sm">
-                    <span className="text-[0.85rem] md:text-[1.25rem]">POST APP</span>
-                </Heading>
+                <Link href="/">
+                    <Heading as="h1" size="sm">
+                        <span className="text-[0.85rem] md:text-[1.25rem]">POST APP</span>
+                    </Heading>
+                </Link>
             </div>
             <div className="md:w-1/3 w-1/2 flex items-center justify-center" id="searchBar">
                 <Input name="search" type="text" placeholder="Search ..." />
