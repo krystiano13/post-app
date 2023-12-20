7e306b2d76d74fa5134fa2efc4ef24da31beaee0
krystiano13 <crystianotv@gmail.com>
2023-12-20 12:02:57 +0100

    implement redirecting to post creation page

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,14 @@
 import { Button } from '@chakra-ui/react';
 import { PostList } from "@/app/components/PostPreview/PostList";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="w-100vw h-auto pt-[5rem] flex flex-col items-center">
-      <Button className="lg:w-1/2 md:w-3/4 w-[80%]">Create Post +</Button>
-      <PostList />
+        <Link className="lg:w-1/2 md:w-3/4 w-[80%]" href="/posts/create">
+            <Button className="w-full">Create Post +</Button>
+        </Link>
+        <PostList />
     </div>
   )
 }
