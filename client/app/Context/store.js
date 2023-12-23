f4c40dc74d9ab00da564a828c99298eaf94ba254
krystiano13 <crystianotv@gmail.com>
2023-12-23 16:31:52 +0100

    move data about user to context provider

--- client/app/Context/store.js
+++ client/app/Context/store.js
@@ -1,15 +1,46 @@
 'use client'
-import { createContext, Dispatch, useContext, SetStateAction, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const GlobalContext = createContext({
     logged: false,
+    username: "Admin"
 });
 
 export function GlobalContextProvider({ children }) {
     const [logged, setLogged] = useState(false);
+    const [username, setUsername] = useState("Admin");
+
+    const pathname = usePathname();
+
+    useEffect(() => {
+        if(!localStorage.getItem('token')) {
+            setLogged(false);
+        }
+
+        fetch('http://127.0.0.1:8000/api/auth/getUser', {
+            headers: {
+                Authorization: `
+                    Bearer ${localStorage.getItem('token')}
+                `
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (!data.message) {
+                    setLogged(true);
+                    setUsername(localStorage.getItem('username'));
+                } else {
+                    setLogged(false);
+                    localStorage.removeItem('token');
+                    localStorage.removeItem("username");
+                }
+            })
+
+    },[pathname]);
 
     return (
-        <GlobalContext.Provider value={{ logged: logged }}>
+        <GlobalContext.Provider value={{ logged: logged, username: username }}>
             {
                 children
             }
