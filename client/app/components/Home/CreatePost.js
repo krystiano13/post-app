497147ca64e39e17741874fc4924514cf13bbe13
krystiano13 <crystianotv@gmail.com>
2023-12-23 16:53:23 +0100

    make homepage use data from global context

--- client/app/components/Home/CreatePost.js
+++ client/app/components/Home/CreatePost.js
@@ -2,42 +2,15 @@
 
 import Link from "next/link";
 import { Button } from "@chakra-ui/react";
-import { useState, useEffect } from 'react';
-import {usePathname} from "next/navigation";
+import { useGlobalContext } from "@/app/Context/store";
 
 export function CreatePost() {
-    const [logged,setLogged] = useState(false);
-    const pathname = usePathname();
-
-    useEffect(() => {
-        if (pathname === '/') {
-            if(!localStorage.getItem('token')) {
-                setLogged(false);
-                return;
-            }
-
-            fetch('http://127.0.0.1:8000/api/auth/getUser', {
-                headers: {
-                    Authorization: `
-                    Bearer ${localStorage.getItem('token')}
-                `
-                }
-            })
-                .then(res => res.json())
-                .then(data => {
-                    if (!data.message) {
-                        setLogged(true);
-                    } else {
-                        setLogged(false);
-                    }
-                })
-        }
-    }, [pathname]);
+    const globalContext = useGlobalContext();
 
     return (
         <>
             {
-                logged &&
+                globalContext.logged &&
                 <Link className="lg:w-1/2 md:w-3/4 w-[80%]" href="/posts/create">
                     <Button className="w-full">Create Post +</Button>
                 </Link>
