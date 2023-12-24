898a47c72ce6177a6b6b3f64ece5d403a67c75f1
krystiano13 <crystianotv@gmail.com>
2023-12-24 13:18:36 +0100

    create Comment model

---
+++ server/app/Models/Comment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /*
+     * table name
+     * @var string
+     */
+    protected $table = 'comments';
+
+    /*
+    * primary key
+    * @var string
+    */
+    protected $primaryKey = 'id';
+
+    /*
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /*
+     * @var array
+     */
+    protected $fillable = [
+        'username',
+        'created_at',
+        'text'
+    ];
+}
