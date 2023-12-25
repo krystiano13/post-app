33e1fe4a0c3597cfe9898764abbbfd0a08072778
krystiano13 <crystianotv@gmail.com>
2023-12-25 11:07:59 +0100

    create api route for adding comments

--- server/app/Http/Controllers/CommentController.php
+++ server/app/Http/Controllers/CommentController.php
@@ -4,15 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
-    public function get(int $postId) {
-        $comments = Comment::where('post_id',$postId) -> get();
+    public function get(int $postId)
+    {
+        $comments = Comment::where('post_id', $postId)->get();
 
         return response([
             'status' => true,
             'comments' => $comments
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request -> all(),[
+            'username' => 'required',
+            'post_id' => 'required',
+            'text' => 'required'
+        ]);
+
+        if($validation -> fails()) {
+            return response([
+                'status' => false,
+                'errors' => $validation -> errors()
+            ], 403);
+        }
+
+        $newComment = Comment::create([
+            'username' => $request -> get('username'),
+            'post_id' => $request -> get('post_id'),
+            'text' => $request -> get('text')
+        ]);
+
+        return response([
+            'status' => true,
+            'message' => 'Comment Created'
         ], 200);
     }
 }
