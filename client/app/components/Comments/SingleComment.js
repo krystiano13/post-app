4671904cbce1a3783012bea2d70895350b5175fc
krystiano13 <crystianotv@gmail.com>
2023-12-27 12:27:54 +0100

    add props to single comment component

--- client/app/components/Comments/SingleComment.js
+++ client/app/components/Comments/SingleComment.js
@@ -1,20 +1,18 @@
 import { Heading, Text, Divider } from "@chakra-ui/react";
 
-export function SingleComment() {
+export function SingleComment({ username, date, text }) {
     return (
         <section id="comment" className="w-full p-4">
             <Heading className="text-start flex items-center">
                 <span className="text-[1.5rem]">
-                    Admin
+                    { username }
                 </span>
                 <span className="ml-2 text-[1rem] font-regular opacity-75">
-                    1 day ago
+                    { date }
                 </span>
             </Heading>
             <Text className="mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam tellus, consectetur sed efficitur
-                sit amet, gravida a libero. Ut imperdiet mollis placerat. Quisque viverra lacus quis nulla pulvinar
-                porta.
+                { text }
             </Text>
             <Divider className="mt-4" />
         </section>
--- client/app/components/Comments/comments.js
+++ client/app/components/Comments/comments.js
@@ -1,14 +1,13 @@
 import { Card, Divider } from "@chakra-ui/react";
 import { SingleComment } from "@/app/components/Comments/SingleComment";
 
-export function Comments({ postId }) {
+export async function Comments({ postId }) {
     return (
         <>
             <div id="dividerWrapper" className="lg:w-1/2 md:w-3/4 w-[80%] mt-12">
                 <Divider />
             </div>
             <Card className="lg:w-1/2 md:w-3/4 w-[80%] mt-6">
-                <SingleComment />
                 <SingleComment />
             </Card>
         </>
