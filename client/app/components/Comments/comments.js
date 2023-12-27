fcb01f6480e2641c6c3356cc0c46b3f166c4470a
krystiano13 <crystianotv@gmail.com>
2023-12-27 12:45:12 +0100

    add animations to the comments components

--- client/app/components/Comments/comments.js
+++ client/app/components/Comments/comments.js
@@ -10,17 +10,16 @@ async function getComments(postId) {
 
 export async function Comments({ postId }) {
     const result = await getComments(postId);
-    console.log(result);
-
     return (
         <>
             <div id="dividerWrapper" className="lg:w-1/2 md:w-3/4 w-[80%] mt-12">
                 <Divider />
             </div>
-            <Card className="lg:w-1/2 md:w-3/4 w-[80%] mt-6">
+            <Card className="lg:w-1/2 md:w-3/4 w-[80%] mt-6 card-anim">
                 {
                     result.comments.map(item => (
                         <SingleComment
+                            key={item.id}
                             username={item.username}
                             text={item.text}
                             date={item.created_at}
