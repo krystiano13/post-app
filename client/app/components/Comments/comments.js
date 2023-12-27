65eee8336947ce30797f5a185e237e3fa2e7f1f4
krystiano13 <crystianotv@gmail.com>
2023-12-27 12:22:39 +0100

    create component of single comment

--- client/app/components/Comments/comments.js
+++ client/app/components/Comments/comments.js
@@ -1,10 +1,16 @@
-import { Divider } from "@chakra-ui/react";
+import { Card, Divider } from "@chakra-ui/react";
+import { SingleComment } from "@/app/components/Comments/SingleComment";
+
 export function Comments({ postId }) {
     return (
         <>
             <div id="dividerWrapper" className="lg:w-1/2 md:w-3/4 w-[80%] mt-12">
                 <Divider />
             </div>
+            <Card className="lg:w-1/2 md:w-3/4 w-[80%] mt-6">
+                <SingleComment />
+                <SingleComment />
+            </Card>
         </>
     )
 }
