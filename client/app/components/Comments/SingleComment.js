c57663d122a492d6953cfa03271ae4ab8b70792b
krystiano13 <crystianotv@gmail.com>
2023-12-28 12:11:26 +0100

    implement showing edit and delete button only when it's user's comment

--- client/app/components/Comments/SingleComment.js
+++ client/app/components/Comments/SingleComment.js
@@ -1,6 +1,8 @@
 import { Heading, Text, Divider, Button } from "@chakra-ui/react";
+import { useGlobalContext } from "@/app/Context/store";
 
 export function SingleComment({ username, date, text }) {
+    const context = useGlobalContext();
     return (
         <section id="comment" className="w-full p-4">
             <Heading className="text-start flex items-center">
@@ -15,8 +17,12 @@ export function SingleComment({ username, date, text }) {
                 { text }
             </Text>
             <Divider className="mt-4" />
-            <Button className="mt-4">Edit</Button>
-            <Button className="ml-2 mt-4">Delete</Button>
+            {
+                context.logged && context.username === username && <>
+                    <Button className="mt-4">Edit</Button>
+                    <Button className="ml-2 mt-4">Delete</Button>
+                </>
+            }
         </section>
     )
 }
