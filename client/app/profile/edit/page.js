f559c58e3cd4a466c77c48c74a48ad6874d57e17
krystiano13 <crystianotv@gmail.com>
2023-12-30 18:38:05 +0100

    create styles for profile editing form

--- client/app/profile/edit/page.js
+++ client/app/profile/edit/page.js
@@ -1,8 +1,9 @@
 'use client'
+
 import { useGlobalContext } from "@/app/Context/store";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
+import {Card, FormLabel, Text, Textarea, Button} from "@chakra-ui/react";
 
 export default function EditProfile() {
     const globalContext = useGlobalContext();
@@ -10,11 +11,19 @@ export default function EditProfile() {
 
     useEffect(() => {
         if(globalContext.logged === false) {
-            router.push('/');
+            //router.push('/');
         }
     },[]);
 
     return (
-        <></>
+        <div className="w-[full] pt-[4rem] h-wout-nav flex items-center text-start justify-center text-amber-50">
+            <Card size="lg" variant="outline" className="p-6 rounded-full form-anim">
+                <form>
+                    <FormLabel className="w-[20rem] mt-6">About Me</FormLabel>
+                    <Textarea className="max-h-[10rem]" required name="about"></Textarea>
+                    <Button className="mt-6 w-full" type="submit">Edit</Button>
+                </form>
+            </Card>
+        </div>
     )
 }
