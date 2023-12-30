68ede77e0167d4fc2f6038597d90aa69ca18fa81
krystiano13 <crystianotv@gmail.com>
2023-12-30 11:46:54 +0100

    implement getting data from database about user profile and error handling that

--- client/app/profile/[username]/page.js
+++ client/app/profile/[username]/page.js
@@ -1,17 +1,32 @@
-import {Avatar, Card, Heading, Text} from "@chakra-ui/react";
+import {Avatar, Card, Heading, Spinner, Text} from "@chakra-ui/react";
+import {Suspense} from "react";
 
-export default function Profile({params}) {
+async function getProfile(username) {
+    const res = await fetch(`http://127.0.0.1:8000/api/profile/${username}`);
+    return res.json();
+}
+
+export default async function Profile({params}) {
+    const prof = await getProfile(params.username);
     return (
         <div className="w-[100vw] h-wout-nav pt-[4rem] flex justify-center items-center">
             <Card className="form-anim p-4 flex flex-col items-center lg:w-[30%] md:w-[40%] w-[70%]" variant="outline">
-                <Avatar className="mt-6"/>
-                <Heading className="mt-6">Admin</Heading>
-                <Text className="text-center mt-6">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra in lacus eget laoreet.
-                    Vestibulum laoreet enim imperdiet, bibendum mi tincidunt, consectetur turpis. Morbi iaculis sagittis
-                    fringilla. Sed magna metus, semper vel volutpat id, ultricies malesuada felis. Sed ornare sodales
-                    nibh in iaculis. Fusce eu maximus tortor. Vestibulum ac nisi massa.
-                </Text>
+                <Suspense fallback={<Spinner/>}>
+                    {
+                        prof.profile.length > 0 ?
+                        <>
+                            <Avatar className="mt-6"/>
+                            <Heading className="mt-6">{prof.profile[0].username}</Heading>
+                            <Text className="text-center mt-6">
+                                {prof.profile[0].about ? prof.profile[0].about : "This profile hasn't got about section created"}
+                            </Text>
+                        </>
+                            :
+                            <Text>
+                                This user doesn't exist
+                            </Text>
+                    }
+                </Suspense>
             </Card>
         </div>
     )
