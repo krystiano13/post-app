4a2a102ef9e55d9da384967164a5842e7a0ad25f
krystiano13 <crystianotv@gmail.com>
2023-12-30 11:33:19 +0100

    add entry animation for profile card

--- client/app/profile/[username]/page.js
+++ client/app/profile/[username]/page.js
@@ -3,7 +3,7 @@ import {Avatar, Card, Heading, Text} from "@chakra-ui/react";
 export default function Profile({params}) {
     return (
         <div className="w-[100vw] h-wout-nav pt-[4rem] flex justify-center items-center">
-            <Card className="p-4 flex flex-col items-center lg:w-[30%] md:w-[40%] w-[70%]" variant="outline">
+            <Card className="form-anim p-4 flex flex-col items-center lg:w-[30%] md:w-[40%] w-[70%]" variant="outline">
                 <Avatar className="mt-6"/>
                 <Heading className="mt-6">Admin</Heading>
                 <Text className="text-center mt-6">
