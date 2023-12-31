b86ed4ce9eb817d6c085fa4fa172540109c26767
krystiano13 <crystianotv@gmail.com>
2023-12-31 15:56:23 +0100

    rebuild showing posts for implementing search

--- client/app/components/PostPreview/PostLIst.js
+++ client/app/components/PostPreview/PostLIst.js
@@ -1,16 +1,38 @@
+'use client'
+
 import { PostPreview } from "@/app/components/PostPreview/PostPreview";
 import { Spinner } from "@chakra-ui/react";
 import {Suspense} from "react";
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-export async function PostList({ posts }) {
+export function PostList() {
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const data = new FormData();
+        data.append("text", "");
+        fetch(`http://127.0.0.1:8000/api/posts/latest/${page}`, {
+            method: "POST",
+            body: data
+        })
+            .then(res => res.json())
+            .then(result => setPosts(prev => result.data))
+            .then(() => {
+                setLoading(false)
+            })
+    }, []);
+
     return (
-        <div className="w-[100vw]">
-            <Suspense fallback={<Spinner />}>
+        <div id={posts.length.toString()} className="w-[100vw]">
                 {
-                    posts.data.map(item => (
+                    loading === false &&
+                    posts.map(item => (
                         <Link className="w-[100vw] flex justify-center" href={`/posts/${item.id}`}>
                             <PostPreview
+                                key={item.id}
                                 title={item.title}
                                 content={item.content}
                                 username={item.username}
@@ -19,7 +41,12 @@ export async function PostList({ posts }) {
                         </Link>
                     ))
                 }
-            </Suspense>
+            {
+                loading === true &&
+                <div className="w-[100vw] h-wout-nav flex items-center justify-center">
+                    <Spinner />
+                </div>
+            }
         </div>
     )
 }
--- client/app/page.js
+++ client/app/page.js
@@ -2,21 +2,11 @@
 import { PostList } from "@/app/components/PostPreview/PostList";
 import { CreatePost } from "@/app/components/Home/CreatePost";
 
-async function getPosts(page = 0, text) {
-    const res = await fetch(`http://127.0.0.1:8000/api/posts/latest/${page}/${text}`, {
-        next: {
-            revalidate: 0
-        }
-    });
-    return res.json();
-}
-
 export default async function Home() {
-    const posts = await getPosts(0,"");
   return (
     <div className="w-100vw h-auto pt-[5rem] flex flex-col items-center overflow-x-hidden">
         <CreatePost />
-        <PostList posts={posts} />
+        <PostList />
     </div>
   )
 }
