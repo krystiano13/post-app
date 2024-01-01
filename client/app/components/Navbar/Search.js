295080a1811e027da6d5ecdf98d0e7bdc656b645
krystiano13 <crystianotv@gmail.com>
2024-01-01 13:40:35 +0100

    remove uneceseary imports from Search component

--- client/app/components/Navbar/Search.js
+++ client/app/components/Navbar/Search.js
@@ -1,11 +1,8 @@
 'use client'
 
 import { Input, Button } from "@chakra-ui/react";
-import { useRouter, useSearchParams } from "next/navigation";
 
 export function Search() {
-    const router = useRouter();
-    const params = useSearchParams();
     function handleSubmit(e) {
         e.preventDefault();
         window.location.href = `/?text=${e.target[0].value}`;
